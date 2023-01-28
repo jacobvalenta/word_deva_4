9304b859575c1d42eef77d315a2827c492b35c50
Jacob Valenta <jacob.valenta@gmail.com>
2023-01-28 12:19:08 -0500

    Term admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from .models import Language, String, Term, Text
+from .models import Language, String, Term, Text, Translation
+
+class TermAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_word', 'count']
+    list_filter = ['text']
+
+    @admin.display(ordering='string__text')
+    def get_word(self, obj):
+        return obj.string.text
 
 admin.site.register(Language)
 admin.site.register(String)
-admin.site.register(Term)
+admin.site.register(Term, TermAdmin)
 admin.site.register(Text)
+admin.site.register(Translation)
