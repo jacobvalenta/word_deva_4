cde0c4913f30f799266e04606d612a2ac94b79ec
Jacob Valenta <jacob.valenta@gmail.com>
2023-01-28 12:18:37 -0500

    Add translation

--- core/models.py
+++ core/models.py
@@ -16,6 +16,23 @@ class String(models.Model):
 	def __str__(self):
 		return self.text
 
+class Translation(models.Model):
+	source_string = models.ForeignKey('String', related_name="source_string",
+									on_delete=models.CASCADE)
+	target_string = models.ForeignKey('String', related_name="target_string",
+								  on_delete=models.CASCADE)
+
+	relevance = models.DecimalField(decimal_places=4, max_digits=5,
+									blank=True, null=True)
+
+	# dictionary = models.ForeignKey('Dictionary', blank=True, null=True,
+	# 							   on_delete=models.CASCADE)
+	notes = models.CharField(max_length=120, blank=True, null=True)
+
+	def __str__(self):
+		return "{0} = {1}".format(self.source_string.text,
+								  self.target_string.text)
+
 class Term(models.Model):
 	string = models.ForeignKey('String', on_delete=models.CASCADE)
 	text = models.ForeignKey('Text', on_delete=models.CASCADE)
@@ -23,7 +40,7 @@ class Term(models.Model):
 	count = models.PositiveIntegerField()
 
 	def __str__(self):
-		return "#{0}: {1}({2})".format(self.order, self.word.text, self.count)
+		return "#{0}: {1}({2})".format(self.order, self.string.text, self.count)
 
 class Text(models.Model):
 	title = models.CharField(max_length=127)
@@ -33,8 +50,10 @@ class Text(models.Model):
 	text = models.TextField()
 
 	word_count = models.PositiveIntegerField(default=0, blank=True, null=True)
-	vocabulary_count = models.PositiveIntegerField(default=0, blank=True, null=True)
-	head_word_count = models.PositiveIntegerField(default=0, blank=True, null=True)
+	vocabulary_count = models.PositiveIntegerField(default=0, blank=True,
+												   null=True)
+	head_word_count = models.PositiveIntegerField(default=0, blank=True,
+												  null=True)
 
 	def __str__(self):
 		return "{0} by {1}".format(self.title, self.author)
