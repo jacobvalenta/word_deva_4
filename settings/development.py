abd0ba5ddb0fa11aefb383d127018a1091ecf99f
Jacob Valenta <jacob.valenta@gmail.com>
2023-01-28 12:26:26 -0500

    Use `DJANGO_ALLOWED_HOSTS` environment variable

--- settings/development.py
+++ settings/development.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.254.40']
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(', ')
 
 
 # Application definition
