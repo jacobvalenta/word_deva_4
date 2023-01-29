199da9b7921f683274101042fdb26cdc34637230
Jacob Valenta <jacob.valenta@gmail.com>
2023-01-29 13:06:54 -0500

    Test Login

--- core/tests.py
+++ core/tests.py
@@ -1,24 +1,27 @@
-from rest_framework.reverse import reverse
-from django.test import TestCase
 from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import resolve
 
+from rest_framework.reverse import reverse
 from rest_framework.test import APIRequestFactory, RequestsClient
-
-from rest_framework_simplejwt.views import (TokenObtainPairView)
 
 class AuthenticationTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create_user(username="test_username", password="test_password")
-        print(user, 'Created')
+        user = User.objects.create_user(username="test_username",
+                                        password="test_password")
 
     def test_login(self):
-        """Test authentication"""
+        """Test JWT authentication"""
         factory = APIRequestFactory()
+        
         login_url = reverse('token_obtain_pair')
+        view = resolve(login_url).func.view_class
+
         request = factory.post(login_url, {'username': 'test_username',
                                            'password': 'test_password'},
                                format="json")
-        response = TokenObtainPairView.as_view()(request)
+        response = view.as_view()(request)
 
-        print(response.status_code)
-        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
